4bdaf752a4a70b0ee61b5381c543a0e1f85a50bb
edwoodward <ecw1@rice.edu>
2020-04-30 16:40:13 -0500

    Added menu and favicon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require semantic-ui
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+})
+
